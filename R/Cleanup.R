345fad23be6558ccf5e63bdce4661eb43d9f6795
bapoorva <bapoorva@mail.med.upenn.edu>
2016-10-13 12:14:51 -0400

    include t-stats column

--- R/Cleanup.R
+++ R/Cleanup.R
@@ -8,7 +8,7 @@
 
 Cleanup <-function(tt){
   res <- tt %>% mutate(fc = ifelse(logFC<0, -1*2^abs(logFC),2^logFC)) %>%
-    select(ENSEMBL,SYMBOL,ENTREZID,biotype,geneloc,logFC,fc,P.Value,adj.P.Val)
+    select(ENSEMBL,SYMBOL,ENTREZID,biotype,geneloc,logFC,fc,P.Value,adj.P.Val,t)
     rownames(res) <- rownames(tt)
     return(res)
 }
