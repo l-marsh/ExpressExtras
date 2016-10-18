b5e8d06a608ab2d25a913d779ea93848649246f8
bapoorva <bapoorva@mail.med.upenn.edu>
2016-10-18 14:11:24 -0400

    load new annotation df

--- R/GeneAnnotate.R
+++ R/GeneAnnotate.R
@@ -21,11 +21,11 @@
 GeneAnnotate <- function(ids,organism="Mm") {
   if(organism=="Mm"){
     #load('~/dsdata/NGSshare/mm9_data/Rdata/Mus_musculus.NCBIM37.67.RData')
-    geneannotation=Mus_musculus.NCBIM37.67.RData$geneannotation
+    geneannotation=Mus_musculus.NCBIM37.67
   }
   else if(organism=="Hs"){
     #load('~/dsdata/NGSshare/hg19_data/RData/gencode.v19.annotation.RData')
-    data('gencode.v19.annotation.RData')
+    geneannotation=gencode.v19.annotation
   }else{
     stop("Wrong organism")
   }
