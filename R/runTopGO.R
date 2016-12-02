87b82bc75f431c0bd0c452451e779d5ee23550ab
Michael Morley <mmorley@mail.med.upenn.edu>
2016-12-02 11:00:35 -0500

    Added Organism switch

--- R/runTopGO.R
+++ R/runTopGO.R
@@ -1,13 +1,30 @@
 #' Return a data frame of gene annotations
 #' Takes a dataframe having output from limma, cleans up to remove unwanted columns and annotates it
 #' @param ids : List of ENSEMBL IDs
-#' @param organism : Mm for Mus musculus (mouse) and Hs for Homo sapiens (humans)
+#' @param organism : mouse for Mus musculus (mouse) and human for Homo sapiens (humans)
 #' @return Dataframe with limma data and the annotation
 #' @import topGO
 #' @export
 
 
-runTopGO <- function(tt,pcutoff=.1){
+runTopGO <- function(tt,organism,pcutoff=.1){
+  if (is.na(organism)) {
+    stop(paste("Please provide an organism human or mouse\n"), call. = FALSE)
+  }
+
+  if(organism=="mouse"){
+    geneannotation="org.Mm.eg.db"
+  }
+  else if(organism=="human"){
+    geneannotation="org.Hs.eg.db"
+  }else{
+    stop("Wrong organism")
+  }
+
+
+
+
+
   geneList <-as.vector(tt$adj.P.Val)
   names(geneList) <- as.character(tt$ENSEMBL)
 
@@ -24,7 +41,7 @@ runTopGO <- function(tt,pcutoff=.1){
                     nodeSize = 10,
                     # annot, tells topGO to map from GO terms to "genes"
                     annot = annFUN.org,
-                    mapping="org.Mm.eg.db",
+                    mapping=geneannotation,
                     ID = "ensembl"
   ), silent=TRUE)
 
